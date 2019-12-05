96cfbee9526a21ec9ab48963260ce18f16871d2f
Ryan Kooy <48595828+ryankooy@users.noreply.github.com>
2019-12-05 10:42:18 -0500

    Update jumbler.cs

--- jumbler.cs
+++ jumbler.cs
@@ -1,12 +1,60 @@
-// using System;
+using System;
+using System.Text;
 
-// class NewName
-// {
-//   public static void Main(string[] args)
-//   {
-//     String[] word = new String[len(word)];
-//     Random w = new Random();
-//     // more logic
-//     Console.WriteLine(w);
-//   }
-// }
+namespace Jumbler
+{
+  public class Program
+  {
+    public int RandNum(int min, int max)
+    {
+      Random random = new Random();
+      return random.Next(min, max);
+    }
+    
+    public string Randomness(int size, bool lowerCase)
+    {
+      StringBuilder builder = new StringBuilder();
+      Random random = new Random();
+      char ch;
+      
+      for (int i = 0; i < size; i++)
+      {
+        ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+        
+        builder.Append(ch);
+      }
+      
+      if (lowerCase)
+        return builder.ToString().ToLower();
+      
+      return builder.ToString();
+    }
+    
+    public string RandomPW()
+    {
+      StringBuilder builder = new StringBuilder();
+      builder.Append(Randomness(4, true));
+      builder.Append(RandNum(1000, 9999));
+      builder.Append(Randomness(2, false));
+      return builder.ToString();
+    }
+  }
+  
+  class Number
+  {
+    static void Main(string[] args)
+    {
+      RandomGenerator generator = new RandomGenerator();
+      int rand = generator.RandomNumber(5, 100);
+      Console.WriteLine($"Random number between 5 and 100 is {rand}.");
+      
+      string str = generator.RandomString(10, false);
+      Console.WriteLine($"Random string of 10 characters is {str}.");
+      
+      string pass = generator.RandomPassword();
+      Console.WriteLine($"Random string of 6 characters is {pass}.");
+      
+      Console.ReadKey();
+    }
+  }
+}
